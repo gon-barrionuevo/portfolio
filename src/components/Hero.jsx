4e6d38ea9011ffe925616be416c98c25206ab1e0
gon-barrionuevo <gonbarrionuevo@gmail.com>
2025-02-19 10:14:30 -0300

    update packages framermotion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { HERO_CONTENT } from "../constants"
 import profilePic from "../assets/gonzaloBarrionuevoProfile.jpg"
+import {motion} from "framer-motion";
 
 export const Hero = () => {
   return (
@@ -7,9 +8,10 @@ export const Hero = () => {
         <div className="flex flex-wrap">
             <div className="w-screen lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1 className="text-white pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+                    <motion.h1 animate={{x:100}}
+                     className="text-white pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
                       Gonzalo Barrionuevo
-                    </h1>
+                    </motion.h1>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
                       SDET & Frontend Developer
                     </span>
